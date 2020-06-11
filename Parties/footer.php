a13330a9714dfff52d62db2a20bf38593c790e12
Louise-Perez <louise.perez.2910@gmail.com>
2020-06-11 23:12:04 +0200

    Mis à jour lien footer

--- Parties/footer.php
+++ Parties/footer.php
@@ -1,15 +1,15 @@
 <footer class="footer">
     <ul>
         <li><strong>Infos Légales</strong></li>
-        <li>Conditions Générale de réservations</li>
-        <li>Mentions Légales</li>
-        <li>Cookies</li>
+        <li><a href="./Incident/construction.php">Conditions Générale de réservations</a></li>
+        <li><a href="./Incident/construction.php">Mentions Légales</a></li>
+        <li><a href="./Incident/construction.php">Cookies</a></li>
     </ul>
     <ul>
         <li><strong>A propos</strong></li>
-        <li>Emploi</li>
-        <li>Partenaires</li>
-        <li>Presse</li>
+        <li><a href="./Incident/construction.php">Emploi</a></li>
+        <li><a href="./Incident/construction.php">Partenaires</a></li>
+        <li><a href="./Incident/construction.php">Presse</a></li>
     </ul>  
     <ul>
         <li><strong>Autres Sites</strong></li>
